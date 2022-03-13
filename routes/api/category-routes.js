e922b3ba95b4120314233c8a9750c063efee95c4
KCaseyChamberlain <caseygchamberlain@gmail.com>
2022-03-12 23:27:02 -0700

    router.put for category functional

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -62,6 +62,25 @@ router.post('/', (req, res) => {
 
 router.put('/:id', (req, res) => {
   // update a category by its `id` value
+
+      // expects {'category_name'}
+    Category.update(req.body, {
+      individualHooks: true,
+      where: {
+          id: req.params.id
+      }
+  })
+      .then(dbCategoryData => {
+          if (!dbCategoryData[0]) {
+              res.status(404).json({ message: 'No category found with this id' });
+              return;
+          }
+          res.json(dbCategoryData);
+      })
+      .catch(err => {
+          console.log(err);
+          res.status(500).json(err);
+      });
 });
 
 router.delete('/:id', (req, res) => {
